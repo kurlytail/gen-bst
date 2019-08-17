073ed614816d3c2a2bc019a03317833d1338e59b
ashwin <ashwinkn@hotmail.com>
2019-08-17 14:14:16 -0500

    generated by     - gen-lib@1.2.22     - @kurlytail/gen-sgen@1.1.22     - @kurlytail/gen-log@1.2.29

---
+++ src/js/__tests__/integration.js
@@ -0,0 +1,23 @@
+import { execSync } from 'child_process';
+
+describe('# integration test', () => {
+    beforeEach(() => {
+        execSync('rm -rf testoutput');
+    });
+
+    it('## should print help options', () => {
+        let output = execSync('npm run build').toString();
+        output = execSync('sgen -g `pwd`/dist/bst.min.js -h').toString();
+        expect(output).toMatchSnapshot();
+    });
+
+    it('## should generate design and run bst commands', () => {
+        let output = execSync('npm run build').toString();
+        output = execSync('sgen -g `pwd`/dist/bst.min.js -d src/test/fixture/design.json -o testoutput').toString();
+        output = output.replace(/info: Loaded generator .*bst.min.js.*/, '');
+        expect(output).toMatchSnapshot();
+        execSync('npm install', { cwd: 'testoutput', stdio: 'inherit' });
+        execSync('npm run lint', { cwd: 'testoutput', stdio: 'inherit' });
+        execSync('npm run build', { cwd: 'testoutput', stdio: 'inherit' });
+    });
+});
